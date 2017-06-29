63599cccf524e72da9efd164974865c0bdadb2f0
Aaron Eshbach <aaron.eshbach@hotmail.com>
2017-06-28 23:30:05 -0400

    Fixed navbar behavior in Blog sample.

--- Samples/CurryOn.Blog/CurryOn.Blog.Web/Scripts/index.js
+++ Samples/CurryOn.Blog/CurryOn.Blog.Web/Scripts/index.js
@@ -1,7 +1,20 @@
 /// <reference path='typings/angularjs/angular.d.ts' />
+// TODO: Add better error handling
 var Blog;
 (function (Blog) {
     var blog = angular.module("blog", []);
+    blog.service("blogUserService", ["$http", function ($http) {
+            var currentUser = { identity: "$anonymous_user", isLoggedIn: false, name: "Anonymous User", email: null };
+            return {
+                currentUser: currentUser,
+                logIn: function (userName, password) {
+                    // TODO: Call server to validate credentials 
+                },
+                register: function (userName, password, email) {
+                    // TODO: Call server to create new user
+                }
+            };
+        }]);
     blog.service("blogPostService", ["$http", function ($http) {
             var selectedPost = { postId: -1, post: null };
             return {
@@ -16,10 +29,15 @@ var Blog;
                 toIndex: function () { return selectedPost.postId = -1; }
             };
         }]);
-    // TODO: Add better error handling
+    blog.controller("blogNavigation", ["$scope", "$http", "blogUserService",
+        function ($scope, $http, userService) {
+            $scope.currentPage = "home";
+            $scope.service = userService;
+            $scope.navigateTo = function (page) { return $scope.currentPage = page; };
+        }]);
     blog.controller("blogHome", ["$scope", "$http", "blogPostService",
-        function ($scope, $http, blogPostService) {
-            $scope.service = blogPostService;
+        function ($scope, $http, postService) {
+            $scope.service = postService;
             $http.get("api/blog").then(function (payload) {
                 $scope.blog = payload.data;
             }, function (error) { return alert(error); });
--- Samples/CurryOn.Blog/CurryOn.Blog.Web/Scripts/index.ts
+++ Samples/CurryOn.Blog/CurryOn.Blog.Web/Scripts/index.ts
@@ -1,19 +1,35 @@
 ﻿/// <reference path='typings/angularjs/angular.d.ts' />
 
+// TODO: Add better error handling
 module Blog {
-
     export interface IBlogPostComment { user: string; comment: string }
     export interface IBlogPost { id: number; title: string; author: string; date: Date; body: string; comments: IBlogPostComment[]; }
     export interface IBlogPostInfo { title: string; author: string; summary: string; date: Date; blogPostId: number; }
     export interface IBlog { name: string; description: string; posts: IBlogPostInfo[]; }
-    export interface ISelectedBlogPost { postId: number; post: IBlogPost;}
+    export interface ISelectedPost { postId: number; post: IBlogPost; }
+    export interface IUser { identity: string; isLoggedIn: boolean; name: string; email: string; }
+    export interface IBlogUserService { currentUser: IUser; logIn: Function; register: Function; }
     export interface IBlogPostService { currentPostId: number; currentPost: IBlogPost; loadBlogPost: Function; toIndex: Function; }
+    export interface IBlogNavigationScope extends ng.IScope { currentPage: string; navigateTo: Function; service: IBlogUserService; }
     export interface IBlogHomeScope extends ng.IScope { blog: IBlog; service: IBlogPostService; }
 
     var blog = angular.module("blog", []);
 
+    blog.service("blogUserService", ["$http", ($http: ng.IHttpService) => {
+        var currentUser = <IUser>{ identity: "$anonymous_user", isLoggedIn: false, name: "Anonymous User", email: null };
+        return <IBlogUserService>{
+            currentUser: currentUser,
+            logIn: (userName: string, password: string) => { 
+                // TODO: Call server to validate credentials 
+            },
+            register: (userName: string, password: string, email: string) => {
+                // TODO: Call server to create new user
+            }
+        };
+    }]);
+
     blog.service("blogPostService", ["$http", ($http: ng.IHttpService) => {
-        var selectedPost = <ISelectedBlogPost>{ postId: -1, post: null };
+        var selectedPost = <ISelectedPost>{ postId: -1, post: null };
 
         return <IBlogPostService>{
             currentPostId: selectedPost.postId,
@@ -28,10 +44,16 @@ module Blog {
         };
     }]);
 
-    // TODO: Add better error handling
+    blog.controller("blogNavigation", ["$scope", "$http", "blogUserService",
+        ($scope: IBlogNavigationScope, $http: ng.IHttpService, userService: IBlogUserService) => {
+            $scope.currentPage = "home";
+            $scope.service = userService;
+            $scope.navigateTo = (page: string) => $scope.currentPage = page;
+        }]);
+    
     blog.controller("blogHome", ["$scope", "$http", "blogPostService",
-        ($scope: IBlogHomeScope, $http: ng.IHttpService, blogPostService: IBlogPostService) => {
-            $scope.service = blogPostService;
+        ($scope: IBlogHomeScope, $http: ng.IHttpService, postService: IBlogPostService) => {
+            $scope.service = postService;
             $http.get("api/blog").then((payload: ng.IHttpPromiseCallbackArg<IBlog>) => {
                 $scope.blog = payload.data;
             }, (error) => alert(error)); 
